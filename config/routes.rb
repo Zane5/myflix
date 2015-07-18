bf58736b7c4eb2ab0498c39f99fc20abcf1935f7
Simon Zheng <fat.fox@gmail.com>
2015-07-18 07:51:45 +0000

    build the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
+  get 'register', to: 'users#new'
+
+  resources :videos, only: [:index, :show] do
+    collection do
+      get 'search', to: 'videos#search'
+    end
+  end
+  resources :categories, only: [:show]
 end
