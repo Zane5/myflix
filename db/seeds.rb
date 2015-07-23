ed8098b759282935e02ea03e644546a45ad7b065
Simon Zheng <fat.fox@gmail.com>
2015-07-23 09:38:02 +0000

    Show Only Recent Videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,62 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-comdey = Category.create(name: "Comdey")
-cartoon =Category.create(name: "Cartoon")
-drama = Category.create(name: "Drama")
+comedies = Category.create(name: "Comedies")
+#cartoon =Category.create(name: "Cartoon")
+dramas = Category.create(name: "Dramas")
+
+futurama = Video.create(title: "Futurama",
+             description: "Futurama is good",
+             cover_small_url: "/tmp/futurama.jpg",
+             cover_large_url: "/tmp/futurama_large.jpg",
+             category: comedies
+            ) 
 monk = Video.create(title: "Monk", 
             description: "Monk is well",
             cover_small_url: "/tmp/monk.jpg",
             cover_large_url: "/tmp/monk_large.jpg",
-            category_id: '1' 
+            category: dramas 
             )
 family_guy = Video.create(title: "Family Guy",
              description: "Family guy is well",
              cover_small_url: "/tmp/family_guy.jpg",
              cover_large_url: "/tmp/family_guy_large.jpg",
-             category_id: '2'
+             category: comedies 
             )
 south_park = Video.create(title: "South Park",
              description: "South Park is well",
              cover_small_url: "/tmp/south_park.jpg",
              cover_large_url: "/tmp/south_park_large.jpg",
-             category_id: '3'
+             category: comedies
             ) 
+
 futurama = Video.create(title: "Futurama",
              description: "Futurama is good",
              cover_small_url: "/tmp/futurama.jpg",
              cover_large_url: "/tmp/futurama_large.jpg",
-             category_id: '1'
+             category: comedies
+            ) 
+monk = Video.create(title: "Monk", 
+            description: "Monk is well",
+            cover_small_url: "/tmp/monk.jpg",
+            cover_large_url: "/tmp/monk_large.jpg",
+            category: dramas 
+            )
+family_guy = Video.create(title: "Family Guy",
+             description: "Family guy is well",
+             cover_small_url: "/tmp/family_guy.jpg",
+             cover_large_url: "/tmp/family_guy_large.jpg",
+             category: comedies 
+            )
+south_park = Video.create(title: "South Park",
+             description: "South Park is well",
+             cover_small_url: "/tmp/south_park.jpg",
+             cover_large_url: "/tmp/south_park_large.jpg",
+             category: comedies
+            ) 
+south_park = Video.create(title: "South Park",
+             description: "South Park is well",
+             cover_small_url: "/tmp/south_park.jpg",
+             cover_large_url: "/tmp/south_park_large.jpg",
+             category: comedies
             ) 
