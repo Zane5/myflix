217d8e743d96030307018b66a52a45cd821bd241
Simon Zheng <fat.fox@gmail.com>
2015-12-19 05:27:50 +0000

    admin sign in

--- config/routes.rb
+++ config/routes.rb
@@ -38,4 +38,8 @@ Myflix::Application.routes.draw do
   get 'expired_token', to: 'pages#expired_token'
 
   resources :invitations, only: [:new, :create]
+
+  namespace :admin do
+    resources :videos, only: [:new, :create]
+  end
 end
